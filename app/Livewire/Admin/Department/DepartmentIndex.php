d7fc39e87dc4fbd0d5e4cc74e50486845cb8e22e
harshali <harshalitaydedweb914@gmail.com>
2023-09-20 15:12:50 +0530

    add edit ,view at department in admin panel

--- app/Livewire/Admin/Department/DepartmentIndex.php
+++ app/Livewire/Admin/Department/DepartmentIndex.php
@@ -11,11 +11,26 @@ class DepartmentIndex extends Component
     use WithPagination;
 
     public $search_input = '';
-    
+    public $isOpen = 0;
+    public $department_name;
+    public $departmentId;
+
     public function search()
     {
         $this->resetPage();
     }
+
+
+    public function openModal()
+    {
+        $this->isOpen = true;
+        $this->resetValidation();
+    }
+    public function closeModal()    
+    {
+        $this->isOpen = false;
+    }
+
 
     public function render()
     {
@@ -24,6 +39,41 @@ class DepartmentIndex extends Component
             'departments' => Departments::where('department_name', 'like', '%' . $this->search_input . '%')->paginate(5)
     ]);
     }
+
+    
+   public function show($id)
+   {
+
+    $department = Departments::where('id', $id)->first();
+    $this->departmentId = $department->id;
+    $this->department_name =  $department->department_name;
+    $this->openModal();
+
+   }
+
+    public function edit($id)
+    {
+        
+        $department = Departments::findOrFail($id);   
+        $this->departmentId = $id;
+        $this-> department_name = $department->department_name;
+        $this->openModal();
+    }
+   
+    public function update()
+    {
+        if ($this->departmentId) {
+            $post = Departments::findOrFail($this->departmentId);
+            $post->update([
+                'department_name' => $this->department_name,
+                
+            ]);
+            session()->flash('success', 'Department updated successfully.');
+            $this->closeModal();
+            $this->reset('department_name', 'departmentId');
+        }  
+    }
+
 
     public function delete($id)
     {
